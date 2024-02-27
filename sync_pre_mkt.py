d164c4b29d0b83e34e774960b443f8f1dfad80a8
Nemo <violet_day@live.com>
2024-02-27 14:13:05 +0800

    update

--- sync_pre_mkt.py
+++ sync_pre_mkt.py
@@ -10,18 +10,17 @@ from common import *
 def job():
     now = get_eastern_now()
     day = now.strftime('%y%m%d')
-    if now.weekday() >= 5:
-        return
-    df = pd.read_csv(f'data/premarket/{day}.csv', names=['time', 'symbol'])
-    df['time'] = df['time'].apply(lambda t: datetime.strptime(t, '%Y-%m-%d %H:%M'))
-    df['date'] = df['time'].apply(lambda t: t.strftime('%Y-%m-%d'))
-    df['time'] = df['time'].apply(lambda t: t.strftime('%Y-%m-%d %H:%M'))
+    if now.weekday() < 5 and now.replace(hour=4, minute=1) <= now <= now.replace(hour=9, minute=25):
+        df = pd.read_csv(f'data/premarket/{day}.csv', names=['time', 'symbol'])
+        df['time'] = df['time'].apply(lambda t: datetime.strptime(t, '%Y-%m-%d %H:%M'))
+        df['date'] = df['time'].apply(lambda t: t.strftime('%Y-%m-%d'))
+        df['time'] = df['time'].apply(lambda t: t.strftime('%Y-%m-%d %H:%M'))
 
-    output = df.groupby(['date', 'symbol']).min('time').reset_index()
-    output = output.groupby(['time']).agg(symbols=('symbol', 'unique')).reset_index()
+        output = df.groupby(['date', 'symbol']).min('time').reset_index()
+        output = output.groupby(['time']).agg(symbols=('symbol', 'unique')).reset_index()
 
-    result = {row['time']: list(row['symbols']) for _, row in output.iterrows()}
-    upload_file(result, f'/quant/premkt/{day}.json')
+        result = {row['time']: list(row['symbols']) for _, row in output.iterrows()}
+        upload_file(result, f'/quant/premkt/{day}.json')
 
 
 if __name__ == '__main__':
