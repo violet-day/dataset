7653fbcde08e5a9b03b268b818cbc7be1b449088
Nemo <violet_day@live.com>
2025-02-04 21:55:56 +0800

    ...

--- sync_github.py
+++ sync_github.py
@@ -8,8 +8,8 @@ import time
 
 def job():
     now = get_eastern_now()
-    # if now.weekday() < 5 and now.replace(hour=9, minute=30) <= now <= now.replace(hour=16, minute=0):
-    if True:
+    if now.weekday() < 5 and now.replace(hour=9, minute=30) <= now <= now.replace(hour=16, minute=0):
+    # if True:
         subprocess.run(['sh', 'sync_github.sh'])
 
 if __name__ == '__main__':
