1428c446b572c856329139a7c61debab88284e05
Nemo <violet_day@live.com>
2025-02-12 17:57:17 +0800

    ...

--- sync_github.py
+++ sync_github.py
@@ -8,14 +8,14 @@ import time
 
 def job():
     now = get_eastern_now()
-    if now.weekday() < 5 and now.replace(hour=9, minute=30) <= now <= now.replace(hour=16, minute=0):
+    if now.weekday() < 5 and now.replace(hour=4, minute=0) <= now <= now.replace(hour=16, minute=0):
     # if True:
         subprocess.run(['sh', 'sync_github.sh'])
 
 if __name__ == '__main__':
     logging.info('hi nemo, pre sync github')
     job()
-    schedule.every(60).seconds.do(job)
+    schedule.every(30).seconds.do(job)
     while True:
         schedule.run_pending()
         time.sleep(1)
