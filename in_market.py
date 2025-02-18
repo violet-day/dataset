9f840e42e542b705bcd2b25d0959189d121e101f
Nemo <violet_day@live.com>
2025-02-18 16:48:27 +0800

    fix time

--- in_market.py
+++ in_market.py
@@ -99,8 +99,8 @@ def job():
     import os
     os.makedirs('data/inmarket/daily/', exist_ok=True)
     os.makedirs('data/inmarket/month/', exist_ok=True)
-    # if now.weekday() < 5 and now.replace(hour=9, minute=30) <= now <= now.replace(hour=16, minute=0):
-    if True:
+    if now.weekday() < 5 and now.replace(hour=9, minute=30) <= now <= now.replace(hour=16, minute=0):
+    # if True:
         logging.info('in market time')
         gainers = top_gainer()
         logging.info(f'fetch top gainer {gainers}')
